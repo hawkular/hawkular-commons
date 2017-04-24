7f8050e763f6eb408bf54fb27278c0ae02406144
Lukas Krejci <metlos@users.noreply.github.com>
2017-04-24 17:43:36 +0200

    Move inventory data model & mixins to commons (#99)
    It has been cleaned from all unnecessary code.
    Both hawkular-agent and hawkular-services rely on this
    
    Also, add ExtendedInventoryStructure to commons api so
    that it can be used in agent and services.

---
+++ hawkular-inventory-parent/hawkular-inventory-api/src/main/java/org/hawkular/inventory/api/model/Environment.java
@@ -0,0 +1,115 @@
+/*
+ * Copyright 2014-2017 Red Hat, Inc. and/or its affiliates
+ * and other contributors as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.hawkular.inventory.api.model;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.Set;
+
+import org.hawkular.inventory.paths.CanonicalPath;
+import org.hawkular.inventory.paths.SegmentType;
+
+import io.swagger.annotations.ApiModel;
+
+/**
+ * An environment is supposed to contain resources that belong to one infrastructure. Examples being "development",
+ * "testing", "staging", "production", etc.
+ *
+ * <p>Note that the environment does not have a dedicated blueprint type (i.e. data required to create a new environment
+ * in some context), because the only data needed to create a new environment is its ID, which can easily be modelled
+ * by a {@code String}.
+ *
+ * @author Lukas Krejci
+ * @since 1.0
+ */
+@ApiModel(description = "Environment can incorporate feeds and can contain resources and metrics.",
+        parent = Entity.class)
+public final class Environment extends Entity {
+
+    public static final SegmentType SEGMENT_TYPE = SegmentType.e;
+
+    @SuppressWarnings("unused")
+    private Environment() {
+    }
+
+    public Environment(CanonicalPath path) {
+        super(path);
+    }
+
+    public Environment(String name, CanonicalPath path) {
+        super(name, path);
+    }
+
+    public Environment(CanonicalPath path, Map<String, Object> properties) {
+        this(null, path, properties);
+    }
+
+    public Environment(String name, CanonicalPath path, Map<String, Object> properties) {
+        super(name, path, properties);
+    }
+
+    @Override
+    public <R, P> R accept(ElementVisitor<R, P> visitor, P parameter) {
+        return visitor.visitEnvironment(this, parameter);
+    }
+
+    @ApiModel("EnvironmentBlueprint")
+    public static final class Blueprint extends Entity.Blueprint {
+
+        public static Builder builder() {
+            return new Builder();
+        }
+
+        //JAXB support
+        @SuppressWarnings("unused")
+        private Blueprint() {
+        }
+
+        public Blueprint(String id) {
+            this(id, Collections.emptyMap());
+        }
+
+        public Blueprint(String id, Map<String, Object> properties) {
+            super(id, properties);
+        }
+
+        public Blueprint(String id, Map<String, Object> properties,
+                         Map<String, Set<CanonicalPath>> outgoing,
+                         Map<String, Set<CanonicalPath>> incoming) {
+            super(id, properties, outgoing, incoming);
+        }
+
+        public Blueprint(String id, String name, Map<String, Object> properties,
+                         Map<String, Set<CanonicalPath>> outgoing,
+                         Map<String, Set<CanonicalPath>> incoming) {
+            super(id, name, properties, outgoing, incoming);
+        }
+
+        @Override
+        public <R, P> R accept(ElementBlueprintVisitor<R, P> visitor, P parameter) {
+            return visitor.visitEnvironment(this, parameter);
+        }
+
+        public static final class Builder extends Entity.Blueprint.Builder<Blueprint, Builder> {
+
+            @Override
+            public Blueprint build() {
+                return new Blueprint(id, name, properties, outgoing, incoming);
+            }
+        }
+    }
+}
---
+++ hawkular-inventory-parent/hawkular-inventory-json-helper/src/main/java/org/hawkular/inventory/json/InventoryJacksonConfig.java
@@ -0,0 +1,109 @@
+/*
+ * Copyright 2014-2017 Red Hat, Inc. and/or its affiliates
+ * and other contributors as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.hawkular.inventory.json;
+
+import org.hawkular.inventory.api.model.AbstractElement;
+import org.hawkular.inventory.api.model.DataEntity;
+import org.hawkular.inventory.api.model.Entity;
+import org.hawkular.inventory.api.model.Environment;
+import org.hawkular.inventory.api.model.Feed;
+import org.hawkular.inventory.api.model.InventoryStructure;
+import org.hawkular.inventory.api.model.Metric;
+import org.hawkular.inventory.api.model.MetricDataType;
+import org.hawkular.inventory.api.model.MetricType;
+import org.hawkular.inventory.api.model.OperationType;
+import org.hawkular.inventory.api.model.Relationship;
+import org.hawkular.inventory.api.model.Resource;
+import org.hawkular.inventory.api.model.ResourceType;
+import org.hawkular.inventory.api.model.StructuredData;
+import org.hawkular.inventory.api.model.Tenant;
+import org.hawkular.inventory.json.mixins.model.AbstractElementMixin;
+import org.hawkular.inventory.json.mixins.model.CanonicalPathMixin;
+import org.hawkular.inventory.json.mixins.model.DataEntityMixin;
+import org.hawkular.inventory.json.mixins.model.EntityBlueprintMixin;
+import org.hawkular.inventory.json.mixins.model.EnvironmentMixin;
+import org.hawkular.inventory.json.mixins.model.FeedMixin;
+import org.hawkular.inventory.json.mixins.model.InventoryStructureMixin;
+import org.hawkular.inventory.json.mixins.model.MetricDataTypeMixin;
+import org.hawkular.inventory.json.mixins.model.MetricMixin;
+import org.hawkular.inventory.json.mixins.model.MetricTypeBlueprintMixin;
+import org.hawkular.inventory.json.mixins.model.MetricTypeMixin;
+import org.hawkular.inventory.json.mixins.model.OperationTypeMixin;
+import org.hawkular.inventory.json.mixins.model.RelationshipMixin;
+import org.hawkular.inventory.json.mixins.model.RelativePathMixin;
+import org.hawkular.inventory.json.mixins.model.ResourceMixin;
+import org.hawkular.inventory.json.mixins.model.ResourceTypeMixin;
+import org.hawkular.inventory.json.mixins.model.StructuredDataMixin;
+import org.hawkular.inventory.json.mixins.model.TenantMixin;
+import org.hawkular.inventory.json.mixins.model.TenantlessCanonicalPathMixin;
+import org.hawkular.inventory.json.mixins.model.TenantlessRelativePathMixin;
+import org.hawkular.inventory.paths.CanonicalPath;
+import org.hawkular.inventory.paths.Path;
+import org.hawkular.inventory.paths.RelativePath;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * A helper class to configure the Jackson's object mapper to correctly serialize and deserialize inventory entities
+ * and paths.
+ *
+ * <p>By default, the paths are serialized and deserialized with the tenant ID retained. There is an option to not
+ * include them in the serialized form by using the {@link TenantlessPathSerializer} and
+ * {@link DetypedPathDeserializer} (by reconfiguring the object mapper to use {@link TenantlessCanonicalPathMixin}
+ * and {@link TenantlessRelativePathMixin} to serialize the {@link CanonicalPath}
+ * or {@link RelativePath} respectively. But beware of the need to pre-configure the deserializer with contextual
+ * information that it will use to inject the tenant id into the deserialized path instances.
+ *
+ * @author Lukas Krejci
+ * @since 0.2.0
+ */
+public final class InventoryJacksonConfig {
+
+    private InventoryJacksonConfig() {
+    }
+
+    /**
+     * Configures the provided object mapper with mixins that define the serialization and deserialization behavior
+     * for inventory entities.
+     *
+     * @param objectMapper the jackson object mapper
+     */
+    public static void configure(ObjectMapper objectMapper) {
+        /**
+         * Model
+         */
+        objectMapper.addMixIn(AbstractElement.class, AbstractElementMixin.class);
+        objectMapper.addMixIn(CanonicalPath.class, CanonicalPathMixin.class);
+        objectMapper.addMixIn(Environment.class, EnvironmentMixin.class);
+        objectMapper.addMixIn(Feed.class, FeedMixin.class);
+        objectMapper.addMixIn(Metric.class, MetricMixin.class);
+        objectMapper.addMixIn(MetricType.class, MetricTypeMixin.class);
+        objectMapper.addMixIn(MetricDataType.class, MetricDataTypeMixin.class);
+        objectMapper.addMixIn(Path.class, CanonicalPathMixin.class);
+        objectMapper.addMixIn(Relationship.class, RelationshipMixin.class);
+        objectMapper.addMixIn(RelativePath.class, RelativePathMixin.class);
+        objectMapper.addMixIn(Resource.class, ResourceMixin.class);
+        objectMapper.addMixIn(ResourceType.class, ResourceTypeMixin.class);
+        objectMapper.addMixIn(Tenant.class, TenantMixin.class);
+        objectMapper.addMixIn(StructuredData.class, StructuredDataMixin.class);
+        objectMapper.addMixIn(DataEntity.class, DataEntityMixin.class);
+        objectMapper.addMixIn(OperationType.class, OperationTypeMixin.class);
+        objectMapper.addMixIn(Entity.Blueprint.class, EntityBlueprintMixin.class);
+        objectMapper.addMixIn(InventoryStructure.class, InventoryStructureMixin.class);
+        objectMapper.addMixIn(MetricType.Blueprint.class, MetricTypeBlueprintMixin.class);
+    }
+}
