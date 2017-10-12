5266ead65cdd7bd2eb9cd92e1823480f15789006
Jay Shaughnessy <jshaughn@redhat.com>
2017-10-12 14:45:42 -0400

    HAWKULAR-1272 Have an out-of-box UI for inspecting inventory Initial version of a UI.  Plenty more to do but this offers basic resource tree navigation and resource type inspection.
    This added the ui war to the feature pack, so it should get picked
    up by the services dist build.
    
    See if grunt-contrib-compass version is the travis build problem.

---
+++ hawkular-inventory-parent/hawkular-inventory-ui/src/main/ui/src/resources/resources-controller.js
@@ -0,0 +1,143 @@
+/*
+ * Copyright 2014-2017 Red Hat, Inc. and/or its affiliates
+ * and other contributors as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+angular.module('hwk.resourcesModule').controller( 'hwk.resourcesController', ['$scope', '$rootScope', '$q', '$modal', 'hwk.resourcesService', 'Notifications',
+  function ($scope, $rootScope, $q, $modal, resourcesService, Notifications) {
+    'use strict';
+
+    console.debug("[Resources] Start: " + new Date());
+
+    var toastError = function (reason) {
+      console.debug('[Resources] Backend error ' + new Date());
+      console.debug(reason);
+      var errorMsg = new Date() + " Status [" + reason.status + "] " + reason.statusText;
+      if (reason.status === -1) {
+        errorMsg = new Date() + " Hawkular Inventory is not responding. Please review browser console for further details";
+      }
+      Notifications.error(errorMsg);
+    };
+
+    $scope.jsonModal = {
+      text: null,
+      title: null,
+      placeholder: null,
+      readOnly: false
+    };
+
+    var updateTree = function () {
+      console.debug("[Resources] refresh tree roots at " + new Date());
+
+      // fetch root resources
+      var promise1 = resourcesService.Roots().query();
+      $q.all([promise1.$promise]).then(function (result) {
+        var resources = result[0].results;
+        resources.sort(function(a,b) {
+          return sortResource(a,b);
+        });
+
+        $scope.tree = [];
+        for (var i = 0; i < resources.length; ++i) {
+          var resource = resources[i];
+          var text = '[' + resource.type.id + '] ' + resource.name;
+          $scope.tree.push( { text: text, lazyLoad: true, resource: resource } );
+        }
+
+        refreshTreeView();
+
+      }, toastError);
+    };
+
+    var refreshTreeView = function () {
+      $('#resourceTree').treeview({
+        collapseIcon: "fa fa-angle-down",
+        data: $scope.tree,
+        lazyLoad: function(n,f) {
+          expandNode(n, f);
+        },
+        expandIcon: "fa fa-angle-right",
+        icon: "fa fa-folder",
+        nodeIcon: "fa fa-folder",
+        showBorder: false
+      });
+    };
+
+    var expandNode = function (node, expanderFunc) {
+
+      // fetch child resources
+      var encodedId = encodeURIComponent(node.resource.id);
+      var promise1 = resourcesService.Children(encodedId).query();
+
+      $q.all([promise1.$promise]).then(function (result) {
+        var resources = result[0].results;
+        resources.sort(function(a,b) {
+          return sortResource(a,b);
+        });
+
+        var branch = [];
+        for (var i = 0; i < resources.length; ++i) {
+          var resource = resources[i];
+          var text = '[' + resource.type.id + '] ' + resource.name;
+          branch.push( { text: text, lazyLoad: true, resource: resource } );
+        }
+        expanderFunc(branch);
+      }, toastError);
+    };
+
+    var sortResource = function (r1,r2) {
+      return r1.type.id.localeCompare(r2.type.id) || r1.name.localeCompare(r2.name);
+    };
+
+    $scope.viewResourceModal = function(resource) {
+      $scope.jsonModal.title = 'View Resource';
+      $scope.jsonModal.placeholder = 'Resource JSON...';
+      $scope.jsonModal.json = angular.toJson(resource,true);
+      $scope.jsonModal.readOnly = true;
+
+      var modalInstance = $modal.open({
+        templateUrl: 'jsonModal.html',
+        backdrop: false, // keep modal up if someone clicks outside of the modal
+        controller: function ($scope, $modalInstance, $log, jsonModal) {
+          $scope.jsonModal = jsonModal;
+          $scope.cancel = function () {
+            $modalInstance.dismiss('cancel');
+          };
+        },
+        resolve: {
+          jsonModal: function () {
+            return $scope.jsonModal;
+          }
+        }
+      });
+    };
+
+    $scope.deleteResource = function(resource) {
+      if (resource) {
+        var promise1 = resourcesService.RemoveResource(resourceId).remove();
+
+        $q.all([promise1.$promise]).then(function (result) {
+          console.debug("[Resources] deleteResourceResult=" + result);
+        }, toastError);
+      }
+    };
+
+    $scope.refreshTree = function() {
+      updateTree();
+    };
+
+    // initial population of root nodes
+    updateTree();
+  }
+]);
